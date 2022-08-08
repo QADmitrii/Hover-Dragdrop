cfa9596d3fd546caf68c35d3c46df75e00d7ec15
Tillerg <mrgish@yandex.ru>
2022-08-08 11:34:30 +0300

    all Tests

--- src/test/java/HoverAndDragDrop.java
+++ src/test/java/HoverAndDragDrop.java
@@ -1,14 +1,12 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverConditions.url;
 
-public class TestPricing {
+public class HoverAndDragDrop {
 
     @BeforeAll
     static void config() {
@@ -24,7 +22,15 @@ public class TestPricing {
         $(".HeaderMenu").$(byText("Compare plans")).click();
         webdriver().shouldHave(url("https://github.com/pricing#compare-features"));
         $(".application-main").shouldHave(text("Choose the plan that’s right for you."));
-
     }
 
+    //Test Drag&Drop
+    @Test
+    void testDragDrop(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        sleep(5000);
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
 }
