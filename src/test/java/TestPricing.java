b2f5c636100389247fa000a07242a8d3789ccc3d
Tillerg <mrgish@yandex.ru>
2022-08-08 10:50:27 +0300

    TestPricing Git

---
+++ src/test/java/TestPricing.java
@@ -0,0 +1,30 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverConditions.url;
+
+public class TestPricing {
+
+    @BeforeAll
+    static void config() {
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browserPosition = "0x0";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void testHoverTest(){
+        open("/");
+        $(".HeaderMenu").$(byText("Pricing")).hover();
+        $(".HeaderMenu").$(byText("Compare plans")).click();
+        webdriver().shouldHave(url("https://github.com/pricing#compare-features"));
+        $(".application-main").shouldHave(text("Choose the plan that’s right for you."));
+
+    }
+
+}
